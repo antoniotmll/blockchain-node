3b8ed3156b9e62c52d155e8f3caaaed0f6f0189a
Antonio <antoniotmll@gmail.com>
2022-03-17 19:17:39 +0100

    fixed bug

--- src/block.js
+++ src/block.js
@@ -2,11 +2,11 @@ const SHA12 = require('crypto-js/sha256');
 
 class Block {
     constructor(data) {
-        this.hash = null,
-            this.height = 0,
-            this.body = (JSON.stringify(data).toString('hex')),
-            this.time = 0,
-            this.previousBlocHash = ''
+        this.hash = null;
+        this.height = 0;
+        this.body = (JSON.stringify(data).toString('hex'));
+        this.time = 0;
+        this.previousBlocHash = "";
     }
 }
 
