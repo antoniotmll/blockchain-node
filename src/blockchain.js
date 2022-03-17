d10373cdb32b177cb02121cee284ecd34c84aa41
Antonio <antoniotmll@gmail.com>
2022-03-17 19:43:50 +0100

    blockchain class added

---
+++ src/blockchain.js
@@ -0,0 +1,32 @@
+const Block = require('./block');
+
+class Blockchain {
+    constructor() {
+        this.chain = [];
+        this.height = -1;
+        this.initializeChain();
+    }
+
+    async initializeChain() {
+        if (this.height === -1) {
+            const block = new Block({ data: 'Genesis Block' });
+            await this.addBlock(block);
+        }
+    }
+
+    addBlock(Block) {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            block.height = self.chain.length;
+            block.time = new Date().getTime().toString().slice();
+
+            if (self.chain.length > 0) {
+                block.previousBlockHash = self.chain[self.chain.length - 1].hash;
+            }
+
+            let errors = await self.ValidateChain();
+        });
+    }
+}
+
+module.exports = Blockchain;
