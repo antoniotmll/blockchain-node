190e429a922823597e1a20e9435384cd538c43bb
Antonio <antoniotmll@gmail.com>
2022-03-17 19:23:49 +0100

    added validate method

--- src/block.js
+++ src/block.js
@@ -8,6 +8,21 @@ class Block {
         this.time = 0;
         this.previousBlocHash = "";
     }
+
+    validate() {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            let currentHash = self.hash;
+
+            self.hash = SHA256(JSON.stringify({...self, hash: null })).toString();
+
+            if (currentHash != self.hash) {
+                return resolve(false);
+            }
+
+            resolve(true);
+        })
+    }
 }
 
 module.exports = Block;
