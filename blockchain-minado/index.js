e00723727a4c7ffeab65af59eb1b6bc727e2d1d4
Antonio <antoniotmll@gmail.com>
2022-04-04 21:13:45 +0200

    show blokcs

--- blockchain-minado/index.js
+++ blockchain-minado/index.js
@@ -1,3 +1,8 @@
 const Block = require("./src/blockchain");
 
 const blockchain = new Blockchain();
+
+for (let i = 0; i < 10; i++) {
+    blockchain.addBlock(`Block ${i}`);
+    console.log(block.toString());
+}
