f2cfef3ca3bd3a8e47a86baabdaaee8faeffde8c
Antonio <antoniotmll@gmail.com>
2022-04-04 21:16:13 +0200

    fix bug

--- blockchain-minado/index.js
+++ blockchain-minado/index.js
@@ -1,8 +1,8 @@
-const Block = require("./src/blockchain");
+const Blockchain = require("./src/blockchain");
 
 const blockchain = new Blockchain();
 
 for (let i = 0; i < 10; i++) {
     blockchain.addBlock(`Block ${i}`);
-    console.log(block.toString());
+    console.log(blockchain.toString());
 }
